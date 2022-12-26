748140990758ee198dab3aa933688f3b3eda1fb2
Pillow <tomahayashi1851@gmail.com>
2022-12-26 10:16:42 +0900

    ABC264_A

---
+++ ABC264/ABC_A.py
@@ -0,0 +1,8 @@
+
+R,L = map(int,input().split())
+if   (R in [0,14]): print("Black")
+elif (R in [1,13] and L in [range(14)]): 
+
+or (R in [3,11] and L in [3, 11]) or (R in [5,9] and L in [5, 9]) or (R in [7,7] and L in [7, 7]) or (R in [8] and L in [8]):
+else:
+    print("Black")
